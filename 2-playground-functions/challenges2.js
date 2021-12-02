5303182f5aa622d2a7d9a1d7ada78ba5ff1b5945
Gudine <gudine283@gmail.com>
2021-12-02 17:40:50 -0300

    Completa requisito 11

--- 2-playground-functions/challenges2.js
+++ 2-playground-functions/challenges2.js
@@ -12,9 +12,44 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function incrementObject(value) {
+  if (value !== undefined) {
+    return value + 1;
+  }
+  return 1;
 }
+
+function maxRepeat(numbers, max) {
+  let repetidos = {};
+  for (let x of numbers) {
+    repetidos[x] = incrementObject(repetidos[x]);
+    if (repetidos[x] >= max) {
+      return true;
+    }
+    if (x < 0 || x > 9) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function generatePhoneNumber(numbers) {
+  if (numbers.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  if (maxRepeat(numbers, 3)) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+
+  let ddd = numbers.slice(0, 2).join('');
+  let first = numbers.slice(2, 7).join('');
+  let last = numbers.slice(7).join('');
+
+  return `(${ddd}) ${first}-${last}`;
+}
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
