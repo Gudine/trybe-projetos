40776eae8f6a9cb6a7931316413d85905c83465e
Gudine <gudine283@gmail.com>
2021-12-02 15:38:31 -0300

    Completa requisito 5

--- 2-playground-functions/challenges.js
+++ 2-playground-functions/challenges.js
@@ -19,8 +19,8 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return wins * 3 + ties;
 }
 
 // Desafio 6
