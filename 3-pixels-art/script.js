799dcaab8ea51e758b1284fb3f1704812fb6961f
Gudine <gudine283@gmail.com>
2021-12-09 17:03:13 -0300

    Realiza 8o requisito e melhora estilo da página

--- 3-pixels-art/script.js
+++ 3-pixels-art/script.js
@@ -7,6 +7,15 @@ const board = document.getElementById('pixel-board');
 // Guarda a cor selecionada numa var
 let selected = document.querySelector('.selected');
 
+// Pinta o pixel alvo do evento dado como parâmetro
+function paintPixel(ev) {
+  const pixel = ev.target;
+  const color = selected.classList[1];
+
+  pixel.className = 'pixel';
+  pixel.classList.add(color);
+}
+
 // Preenche a pixel-board com uma grade de pixels,
 // usando os valores de largura e altura definidos
 function fillBoard() {
@@ -14,6 +23,7 @@ function fillBoard() {
     const pixel = document.createElement('div');
     pixel.classList.add('pixel');
     pixel.classList.add('blank');
+    pixel.addEventListener('click', paintPixel);
     board.appendChild(pixel);
 
     if (i % w === w - 1) {
@@ -23,16 +33,20 @@ function fillBoard() {
 }
 
 function selectColor(color) {
-  if (selected) { selected.classList.remove("selected"); }
+  if (selected) { selected.classList.remove('selected'); }
   selected = color;
-  selected.classList.add("selected");
+  selected.classList.add('selected');
 }
 
 // Passa por todas as cores e adiciona um evento
 // quando clicadas, que adiciona a classe selected.
 // (O Lint não gosta de for/of, aparentemente)
 colors.forEach(
-  (color) => color.addEventListener("click",
+  (color) => color.addEventListener('click',
+    (ev) => selectColor(ev.target)),
+);
+colors.forEach(
+  (color) => color.addEventListener('click',
     (ev) => selectColor(ev.target)),
 );
 
