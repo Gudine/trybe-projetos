afea930509d63d0d69659ad1d8e0d2014faa71d4
Gudine <gudine283@gmail.com>
2021-12-09 15:44:19 -0300

    Realiza 6o requisito

--- 3-pixels-art/script.js
+++ 3-pixels-art/script.js
@@ -1,19 +1,33 @@
-const board = document.getElementById("pixel-board");
-
-//Declara largura e altura, respectivamente
+// Declara largura e altura, respectivamente
 const [w, h] = [5, 5];
+// Faz lista de cores da paleta
+const colors = document.querySelectorAll('.color');
+// Guarda o quadro de pixels numa var
+const board = document.getElementById('pixel-board');
+// Guarda a cor selecionada numa var
+let selected = document.querySelector('.selected');
 
 function fillBoard() {
   for (let i = 0; i < w * h; i += 1) {
-    const pixel = document.createElement("div");
-    pixel.classList.add("pixel");
-    pixel.classList.add("blank");
+    const pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    pixel.classList.add('blank');
     board.appendChild(pixel);
 
     if (i % w === w - 1) {
-      board.appendChild(document.createElement("br"));
+      board.appendChild(document.createElement('br'));
     }
   }
 }
 
+function selectColor(color) {
+  if (selected) { selected.classList.remove("selected"); }
+  selected = color;
+  selected.classList.add("selected");
+}
+
 fillBoard();
+
+if (!selected) {
+  selectColor(colors[0]);
+}
