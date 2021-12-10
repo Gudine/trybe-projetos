85ed86da3e253ba52ef46fd3ff97e655d3b1d947
Gudine <gudine283@gmail.com>
2021-12-10 15:44:41 -0300

    Realiza 7o e 8o requisito

--- 4-todo-list/script.js
+++ 4-todo-list/script.js
@@ -2,9 +2,19 @@ const input = document.getElementById('texto-tarefa');
 const button = document.getElementById('criar-tarefa');
 const ol = document.getElementById('lista-tarefas');
 
-function createTask(ev) {
+function selectTask(ev) {
+  const selected = document.querySelector('.selected');
+  if (selected) {
+    selected.classList.remove('selected');
+  }
+  ev.target.classList.add('selected');
+}
+
+function createTask() {
   const li = document.createElement('li');
   li.innerText = input.value;
+  li.addEventListener('click', selectTask);
+
   ol.appendChild(li);
   input.value = '';
 }
