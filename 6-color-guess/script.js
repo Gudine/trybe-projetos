0bd0e5a248625f5431f805f210846166920beb57
Gudine <gudine283@gmail.com>
2021-12-15 20:50:08 -0300

    Realiza 5o requisito

--- 6-color-guess/script.js
+++ 6-color-guess/script.js
@@ -1,4 +1,7 @@
+const colorPrompt = document.getElementById('rgb-color');
 const colorSect = document.getElementById('colors');
+const answerSpan = document.getElementById('answer');
+
 const colors = [];
 let answer = null;
 
@@ -9,6 +12,23 @@ function generateColor() {
   return { r, g, b, full: `rgb(${r}, ${g}, ${b})` };
 }
 
+function rightAnswer() {
+  answerSpan.innerText = 'Acertou!';
+}
+
+function wrongAnswer() {
+  answerSpan.innerText = 'Errou! Tente novamente!';
+}
+
+function findAnswer(ev) {
+  const currColor = ev.target.style.backgroundColor;
+  if (currColor === colors[answer].full) {
+    rightAnswer();
+  } else {
+    wrongAnswer();
+  }
+}
+
 function genColorBalls() {
   colors.filter(() => false);
   for (let i = 0; i < 6; i += 1) {
@@ -17,10 +37,13 @@ function genColorBalls() {
 
     div.classList.add('ball');
     div.style.backgroundColor = color.full;
+    div.addEventListener('click', findAnswer);
+
     colorSect.appendChild(div);
     colors.push(color);
   }
   answer = Math.floor(Math.random() * (6));
+  colorPrompt.innerText = colors[answer].full.slice(3);
 }
 
 genColorBalls();
