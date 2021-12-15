edd591c133e05ecf1a75e0a0b5e2080e47b07580
Gudine <gudine283@gmail.com>
2021-12-15 15:23:38 -0300

    Realiza 2o requisito

--- 5-meme-generator/script.js
+++ 5-meme-generator/script.js
@@ -1,8 +1,24 @@
 const textInput = document.getElementById('text-input');
+const imgInput = document.getElementById('meme-insert');
+
 const textDisplay = document.getElementById('meme-text');
+const imgDisplay = document.getElementById('meme-image');
+
 
 function printMemeText() {
   textDisplay.innerText = textInput.value;
 }
 
+function fileUpload(ev) {
+  const file = ev.target.files[0];
+  const reader = new FileReader();
+
+  reader.addEventListener('load',
+    (e) => {
+      imgDisplay.src = e.target.result;
+    });
+  reader.readAsDataURL(file);
+}
+
 textInput.addEventListener('keyup', printMemeText);
+imgInput.addEventListener('change', fileUpload);
