b930c7b8deca54bd2017b5e9f2dd493e3ca4fc22
Gudine <gudine283@gmail.com>
2021-12-15 21:23:46 -0300

    Realiza 7o requisito

--- 6-color-guess/script.js
+++ 6-color-guess/script.js
@@ -2,6 +2,7 @@ const colorPrompt = document.getElementById('rgb-color');
 const colorSect = document.getElementById('colors');
 const answerSpan = document.getElementById('answer');
 const resetButton = document.getElementById('reset-game');
+const score = document.getElementById('score');
 
 const colors = [];
 let answer = null;
@@ -13,17 +14,26 @@ function generateColor() {
   return { r, g, b, full: `rgb(${r}, ${g}, ${b})` };
 }
 
-function findAnswer(ev) {
+function changeScore(diff) {
+  let scoreNum = parseInt(score.innerText, 10);
+  scoreNum += diff;
+  if (scoreNum < 0) { scoreNum = 0; }
+  score.innerText = scoreNum;
+}
+
+function selectColor(ev) {
   ev.target.classList.add('selected');
   const currColor = ev.target.style.backgroundColor;
   if (currColor === colors[answer].full) {
     answerSpan.innerText = 'Acertou!';
+    changeScore(3);
   } else {
     answerSpan.innerText = 'Errou! Tente novamente!';
+    changeScore(-1);
   }
 
   [...document.getElementsByClassName('ball')].forEach(
-    (elem) => elem.removeEventListener('click', findAnswer),
+    (elem) => elem.removeEventListener('click', selectColor),
   );
 }
 
@@ -34,7 +44,7 @@ function genColorBalls() {
 
     div.classList.add('ball');
     div.style.backgroundColor = color.full;
-    div.addEventListener('click', findAnswer);
+    div.addEventListener('click', selectColor);
 
     colorSect.appendChild(div);
     colors.push(color);
