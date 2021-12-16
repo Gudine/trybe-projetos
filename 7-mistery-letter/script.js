30337b0337ce3f2af1b504059cbaf6599f2f238e
Gudine <gudine283@gmail.com>
2021-12-16 19:11:32 -0300

    Realiza 5o requisito

--- 7-mistery-letter/script.js
+++ 7-mistery-letter/script.js
@@ -1,14 +1,22 @@
+const spaceRegex = /^\s+$/;
 const letterText = document.getElementById('carta-texto');
 const makeLetterBtn = document.getElementById('criar-carta');
 const letterP = document.getElementById('carta-gerada');
 
 function removeWords() {
-  const spans = [...document.querySelectorAll('#carta-gerada span')];
-  spans.forEach((span) => span.remove());
+  const elems = [...document.querySelectorAll('#carta-gerada > *')];
+  elems.forEach((elem) => elem.remove());
 }
 
 function generateLetter() {
   removeWords();
+  if (letterText.value.match(spaceRegex)) {
+    const warning = document.createElement('span');
+    warning.classList.add('warning');
+    warning.innerText = 'Por favor, digite o conteúdo da carta.';
+    letterP.appendChild(warning);
+    return;
+  }
   const words = letterText.value.split(' ');
   words.forEach(
     (word) => {
