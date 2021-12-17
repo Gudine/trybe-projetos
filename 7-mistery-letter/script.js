44108bc73b576b130b394b5916a1e5326164c360
Gudine <gudine283@gmail.com>
2021-12-17 00:47:18 -0300

    Realiza os requisitos 16, 17 e 18

--- 7-mistery-letter/script.js
+++ 7-mistery-letter/script.js
@@ -1,5 +1,11 @@
 const spaceRegex = /^\s+$/;
+const styleList = ['newspaper', 'magazine1', 'magazine2'];
+const sizeList = ['medium', 'big', 'reallybig'];
+const rotationList = ['rotateleft', 'rotateright'];
+const skewList = ['skewleft', 'skewright'];
+
 const letterText = document.getElementById('carta-texto');
+const letterCounter = document.getElementById('carta-contador');
 const makeLetterBtn = document.getElementById('criar-carta');
 const letterP = document.getElementById('carta-gerada');
 
@@ -8,23 +14,46 @@ function removeWords() {
   elems.forEach((elem) => elem.remove());
 }
 
+function generateWarning() {
+  const warning = document.createElement('span');
+  warning.classList.add('warning');
+  warning.innerText = 'Por favor, digite o conteúdo da carta.';
+  letterP.appendChild(warning);
+}
+
+function getRandomFromArray(array) {
+  const random = Math.floor(Math.random() * (array.length));
+  return array[random];
+}
+
+function randomizeSpanStyle(span) {
+  [...span.classList].forEach((c) => span.classList.remove(c));
+  span.classList.forEach(() => span.classList.remove());
+  [styleList, sizeList, rotationList, skewList].forEach(
+    (list) => {
+      const c = getRandomFromArray(list);
+      if (c !== undefined) { span.classList.add(c); }
+    },
+  );
+}
+
+function generateSpan(word) {
+  const span = document.createElement('span');
+  span.innerText = word;
+  randomizeSpanStyle(span);
+  span.addEventListener('click', (ev) => randomizeSpanStyle(ev.target));
+  letterP.appendChild(span);
+}
+
 function generateLetter() {
   removeWords();
   if (letterText.value.match(spaceRegex)) {
-    const warning = document.createElement('span');
-    warning.classList.add('warning');
-    warning.innerText = 'Por favor, digite o conteúdo da carta.';
-    letterP.appendChild(warning);
+    generateWarning();
     return;
   }
   const words = letterText.value.split(' ');
-  words.forEach(
-    (word) => {
-      const span = document.createElement('span');
-      span.innerText = word;
-      letterP.appendChild(span);
-    },
-  );
+  letterCounter.innerText = words.length;
+  words.forEach(generateSpan);
 }
 
 makeLetterBtn.addEventListener('click', generateLetter);
