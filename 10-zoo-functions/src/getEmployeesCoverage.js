9b23312a75def134f0769aa3bea3150bc72adbd7
Gudine <gudine283@gmail.com>
2022-01-25 18:52:25 -0300

    Realiza 10o requisito

--- 10-zoo-functions/src/getEmployeesCoverage.js
+++ 10-zoo-functions/src/getEmployeesCoverage.js
@@ -1,7 +1,39 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const getEmployee = (query) => {
+  if (query.id !== undefined) {
+    return data.employees
+      .find((employee) => employee.id === query.id);
+  }
+  return data.employees
+    .find((employee) => employee.firstName === query.name || employee.lastName === query.name);
+};
+
+const getSingleEmployeeCoverage = (employee) => {
+  if (employee === undefined) {
+    throw new Error('Informações inválidas');
+  }
+
+  const { id, firstName, lastName, responsibleFor } = employee;
+
+  const speciesList = responsibleFor
+    .map((speciesId) => data.species
+      .find((spec) => spec.id === speciesId));
+  return {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: speciesList.map((species) => species.name),
+    locations: speciesList.map((species) => species.location),
+
+  };
+};
+
+const getEmployeesCoverage = (options) => {
+  if (options === undefined || (options.id === undefined && options.name === undefined)) {
+    return data.employees
+      .reduce((list, employee) => list.concat([getSingleEmployeeCoverage(employee)]), []);
+  }
+  return getSingleEmployeeCoverage(getEmployee(options));
+};
 
 module.exports = getEmployeesCoverage;
