a2877e62e1c0d423a6b5fc7f1732b4df8e13b90a
Gudine <gudine283@gmail.com>
2022-01-25 18:15:59 -0300

    Realiza 9o requisito

--- 10-zoo-functions/src/getOldestFromFirstSpecies.js
+++ 10-zoo-functions/src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const getOldestFromFirstSpecies = (id) => {
+  const employee = data.employees.find((emp) => emp.id === id);
+  const speciesId = employee.responsibleFor
+    .find((specId) => data.species
+      .some((spec) => spec.id === specId));
+  const species = data.species.find((spec) => spec.id === speciesId);
+  const oldest = species.residents.reduce((old, resid) => (resid.age > old.age ? resid : old));
+  return Object.values(oldest);
+};
 
 module.exports = getOldestFromFirstSpecies;
