1b217c075373ab7f3495a6b7ab4346d8f1ac1f47
Gudine <gudine283@gmail.com>
2022-01-25 17:16:16 -0300

    Realiza 7o requisito

--- 10-zoo-functions/src/getAnimalMap.js
+++ 10-zoo-functions/src/getAnimalMap.js
@@ -1,7 +1,35 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+const getSingleElem = (animal, { includeNames, sex, sorted }) => {
+  if (!includeNames) {
+    return animal.name;
+  }
+
+  const elem = { [animal.name]: animal.residents };
+
+  if (sex !== undefined) {
+    elem[animal.name] = elem[animal.name]
+      .filter((indiv) => indiv.sex === sex);
+  }
+
+  elem[animal.name] = elem[animal.name].map((indiv) => indiv.name);
+
+  if (sorted) {
+    elem[animal.name].sort();
+  }
+
+  return elem;
+};
+
+const getAnimalMap = (options) => data.species.reduce((acc, animal) => {
+  const result = { ...acc };
+  if (options !== undefined) {
+    result[animal.location].push(getSingleElem(animal, options));
+  } else {
+    result[animal.location].push(animal.name);
+  }
+
+  return result;
+}, { NE: [], NW: [], SE: [], SW: [] });
 
 module.exports = getAnimalMap;
