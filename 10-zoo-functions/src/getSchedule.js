af15492302521b7ed1a237a6576105540ca8257b
Gudine <gudine283@gmail.com>
2022-01-25 17:58:47 -0300

    Realiza 8o requisito

--- 10-zoo-functions/src/getSchedule.js
+++ 10-zoo-functions/src/getSchedule.js
@@ -1,7 +1,34 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
-}
+const getFullSchedule = () => Object.entries(data.hours)
+  .reduce((list, [weekday, { open, close }]) => {
+    const output = list;
+    output[weekday] = {
+      officeHour: `Open from ${open}am until ${close}pm`,
+      exhibition: data.species
+        .filter((animal) => animal.availability.includes(weekday))
+        .map((animal) => animal.name),
+    };
+    if (weekday === 'Monday') {
+      output[weekday] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+    }
+    return output;
+  }, {});
+
+const getSchedule = (scheduleTarget) => {
+  if (data.species.some((animal) => animal.name === scheduleTarget)) {
+    return data.species.find((animal) => animal.name === scheduleTarget).availability;
+  }
+
+  const schedule = getFullSchedule();
+  if (Object.keys(schedule).includes(scheduleTarget)) {
+    return { [scheduleTarget]: schedule[scheduleTarget] };
+  }
+
+  return schedule;
+};
 
 module.exports = getSchedule;
