d4cfc922f3eb39d6e790eabe2de5b69c60434ba2
Gudine <gudine283@gmail.com>
2022-02-05 20:21:04 -0300

    Adiciona acesso a dados e restrutura código

--- 10-zoo-functions/script.js
+++ 10-zoo-functions/script.js
@@ -1,5 +1,7 @@
+const clearMatch = /^(clear|console\.clear\(\))$/;
 const mainMatch = /^(\w+)\(([^)]*)\)(?:(?:\[[^\]]+\])|(?:\.\w+))*$/;
-const keysMatch = /(?<=^(?:\w+)\((?:[^)]*)\).*)(?:(?:\[([^\]]+)\])|(?:\.(\w+)))/g;
+const dataMatch = /^data(?:(?:\[[^\]]+\])|(?:\.\w+))*$/;
+const keysMatch = /(?:(?:\[([^\]]+)\])|(?:\.(\w+)))/g;
 const prompt = document.querySelector('.prompt');
 const animals = ['🐒', '🦍', '🦧', '🐕', '🐺', '🦊', '🦝', '🐈', '🦁', '🐅', '🐎', '🦄', '🦓', '🦌', '🦬', '🐂', '🐄', '🐖', '🐗', '🐏', '🐑', '🐐', '🐪', '🦙', '🦒', '🐘', '🦣', '🦏', '🦛', '🐁', '🐹', '🐇', '🐿️', '🦫', '🦔', '🦇', '🐻', '🐨', '🐼', '🦥', '🦦', '🦨', '🦘', '🦡', '🦃', '🐔', '🐓', '🐦', '🐧', '🕊️', '🦅', '🦆', '🦢', '🦉', '🦤', '🦩', '🦚', '🦜', '🐸', '🐊', '🐢', '🦎', '🐍', '🐉', '🐳', '🐬', '🦭', '🐟', '🐠', '🐡', '🦈', '🐙', '🐌', '🦋', '🐛', '🐜', '🐝', '🪲', '🐞', '🦗', '🪳', '🕷️', '🦂', '🦟', '🪰', '🪱'];
 
@@ -17,62 +19,7 @@ const functDict = {
   getEmployeesCoverage, 
 };
 
-function execFunction(match) {
-  if (!match) return "ERRO: Formatação invalida."
-  let funct, params;
-  
-  try {
-    funct = functDict[match[1]];
-  } catch {
-    return "ERRO: Função não reconhecida.";
-  }
-  
-  try {
-    params = JSON.parse(`[${match[2]}]`);
-  } catch {
-    return "ERRO: Parâmetros inválidos.";
-  }
-
-  let result = funct(...params);
-  const keys = [...match.input.matchAll(keysMatch)].map((key) => key[1] || `"${key[2]}"`);
-  try {
-    keys.forEach((key) => {
-      result = result[JSON.parse(`[${key}]`)[0]];
-    });
-  } catch {
-    return "ERRO: Parâmetros inválidos.";
-  }
-
-  return result;
-}
-
-function sendCommand(ev) {
-  if (ev.key !== 'Enter') { return; }
-  ev.preventDefault();
-
-  ev.target.contentEditable = false;
-
-  const match = ev.target.innerText.match(mainMatch);
-  generateOutput(JSON.stringify(execFunction(match), null, 2));
-  generateInput();
-}
-
-function overridePaste(ev) {
-  // Código adaptado de:
-  // https://stackoverflow.com/questions/12027137/
-  ev.preventDefault();
-  let text = ev.clipboardData.getData('text/plain');
-
-  // Código adaptado de:
-  // https://stackoverflow.com/questions/2920150/
-  const sel = window.getSelection();
-  const range = sel.getRangeAt(0);
-  range.deleteContents();
-  range.insertNode(document.createTextNode(text));
-  range.setStart(range.startContainer, range.endOffset);
-}
-
-function generateInput() {
+const generateInput = () => {
   const p = document.createElement('p');
   p.classList.add('input');
 
@@ -87,14 +34,90 @@ function generateInput() {
   const range = document.createRange();
   span.focus();
   range.selectNodeContents(span);
-}
+};
 
-function generateOutput(text) {
+const generateOutput = (text) => {
   const p = document.createElement('p');
   p.classList.add('output');
   p.innerText = text;
   prompt.appendChild(p);
+};
+
+const getKeys = (base, match) => {
+  const keys = [...match.input.matchAll(keysMatch)].map((key) => key[1] || `"${key[2]}"`);
+  try {
+    keys.forEach((key) => {
+      base = base[JSON.parse(`[${key}]`)[0]];
+    });
+  } catch {
+    return "ERRO: Parâmetros inválidos.";
+  }
+
+  return base;
 }
+
+const getData = (match) => {
+  if (!match) return "ERRO: Formatação invalida.";
+  
+  return getKeys(data, match);
+}
+
+const execFunction = (match) => {
+  if (!match) return "ERRO: Formatação invalida.";
+
+  let funct, params;
+  
+  try {
+    funct = functDict[match[1]];
+  } catch {
+    return "ERRO: Função não reconhecida.";
+  }
+  
+  try {
+    params = JSON.parse(`[${match[2]}]`);
+  } catch {
+    return "ERRO: Parâmetros inválidos.";
+  }
+
+  return getKeys(funct(...params), match);
+};
+
+const sendCommand = (ev) => {
+  if (ev.key !== 'Enter') return;
+  ev.preventDefault();
+
+  ev.target.contentEditable = false;
+  
+  const text = ev.target.innerText;
+  let result;
+
+  if (text.match(clearMatch)) {
+    prompt.innerHTML = '';
+    generateInput();
+    return;
+  };
+
+  if (text.match(dataMatch)) result = getData(text.match(dataMatch));
+  else result = execFunction(text.match(mainMatch));
+
+  generateOutput(JSON.stringify(result, null, 2));
+  generateInput();
+};
+
+const overridePaste = (ev) => {
+  // Código adaptado de:
+  // https://stackoverflow.com/questions/12027137/
+  ev.preventDefault();
+  let text = ev.clipboardData.getData('text/plain');
+
+  // Código adaptado de:
+  // https://stackoverflow.com/questions/2920150/
+  const sel = window.getSelection();
+  const range = sel.getRangeAt(0);
+  range.deleteContents();
+  range.insertNode(document.createTextNode(text));
+  range.setStart(range.startContainer, range.endOffset);
+};
 
 randomizeAnimals();
 generateInput();
