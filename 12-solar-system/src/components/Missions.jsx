dc0970d4569bb8d0370f1071fba113f9d82734bc
Gudine <gudine283@gmail.com>
2022-02-18 15:32:00 -0300

    feat: Cria uma lista de missões no componente Missions, usando o componente MissionCard

--- 12-solar-system/src/components/Missions.jsx
+++ 12-solar-system/src/components/Missions.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends Component {
   render() {
+    const mapFunct = (mission) => (
+      <MissionCard
+        key={ mission.name }
+        name={ mission.name }
+        year={ mission.year }
+        country={ mission.country }
+        destination={ mission.destination }
+      />
+    );
+
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missions.map(mapFunct)}
       </div>
     );
   }
