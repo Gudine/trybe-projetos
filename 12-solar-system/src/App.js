91bcdf470457405cd53779442ee704115d7a0845
Gudine <gudine283@gmail.com>
2022-02-18 14:58:28 -0300

    feat: Cria novo componente SolarSystem e o integra no App

--- 12-solar-system/src/App.js
+++ 12-solar-system/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <SolarSystem />
+      </>
     );
   }
 }
---
+++ 12-solar-system/src/components/SolarSystem.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system" />
+    );
+  }
+}
+
+export default SolarSystem;
