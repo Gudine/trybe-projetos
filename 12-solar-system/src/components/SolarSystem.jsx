1b0a7dd6d8a3291b4cafccb5ea1a7cba8f2397aa
Gudine <gudine283@gmail.com>
2022-02-18 15:14:50 -0300

    feat: Cria uma lista de planetas no componente Solar System, usando o componente PlanetCard

--- 12-solar-system/src/components/SolarSystem.jsx
+++ 12-solar-system/src/components/SolarSystem.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends Component {
   render() {
+    const mapFunct = (planet) => (
+      <PlanetCard planetName={ planet.name } planetImage={ planet.image } />
+    );
+
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map(mapFunct)}
       </div>
     );
   }
