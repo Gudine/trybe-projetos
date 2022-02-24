98aca687e9b4273c37b0f092395fbb57db45dda1
Gudine <gudine283@gmail.com>
2022-02-24 15:43:33 -0300

    fix: remove arquivo desnecessário e troca tipo de botão de salvar

--- 13-tryunfo/src/components/Form.jsx
+++ 13-tryunfo/src/components/Form.jsx
@@ -86,7 +86,7 @@ class Form extends Component {
           testid="trunfo-input"
         />)}
         <button
-          type="submit"
+          type="button"
           disabled={ isSaveButtonDisabled }
           onClick={ onSaveButtonClick }
           data-testid="save-button"
