5b825039ba53c3c30cc6e5a5d1133da483ab0fd1
Gudine <gudine283@gmail.com>
2022-03-07 16:19:00 -0300

    feat: Adiciona links de navegação ao Header

--- 14-trybetunes/src/components/Header.jsx
+++ 14-trybetunes/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 import './Header.css';
@@ -25,9 +26,15 @@ class Header extends Component {
     return (
       <header data-testid="header-component">
         {loading && <Loading />}
-        <div>TrybeTunes</div>
-
-        <div data-testid="header-user-name">{username}</div>
+        <section>
+          <div>TrybeTunes</div>
+          <div data-testid="header-user-name">{username}</div>
+        </section>
+        <nav>
+          <Link data-testid="link-to-search" to="/search">Search</Link>
+          <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+          <Link data-testid="link-to-profile" to="/profile">Profile</Link>
+        </nav>
       </header>
     );
   }
