9f15e2acaafb12b0ea42fd1391a944163b54c944
Gudine <gudine283@gmail.com>
2022-03-07 15:28:59 -0300

    feat: Adiciona componente Loading e completa página de Login

---
+++ 14-trybetunes/src/components/Loading.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Loading.css';
+
+class Loading extends Component {
+  render() {
+    return (
+      <div className="loading">
+        Carregando...
+      </div>
+    );
+  }
+}
+
+export default Loading;
--- 14-trybetunes/src/pages/Login.jsx
+++ 14-trybetunes/src/pages/Login.jsx
@@ -1,8 +1,63 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: '',
+      loading: false,
+      sent: false,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, type } = target;
+    const value = type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleForm = (ev) => {
+    ev.preventDefault();
+    const { username } = this.state;
+
+    this.setState({ loading: true });
+    createUser({ name: username })
+      .then(() => this.setState({ loading: false, sent: true }));
+  }
+
   render() {
-    return (<div data-testid="page-login" />);
+    const { username, loading, sent } = this.state;
+    const MIN_USER_LENGTH = 3;
+
+    return (
+      <div data-testid="page-login">
+        {sent && <Redirect to="/search" />}
+        {loading && <Loading />}
+        <form onSubmit={ this.handleForm }>
+          <input
+            data-testid="login-name-input"
+            type="text"
+            name="username"
+            value={ username }
+            onChange={ this.handleChange }
+            placeholder="Nome de Usuário"
+          />
+          <button
+            data-testid="login-submit-button"
+            type="submit"
+            disabled={ username.length < MIN_USER_LENGTH }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
