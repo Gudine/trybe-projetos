2cd2d2cd19704bc9de462321969f3701a74a83f1
Lalá Nametala <lalanametala@gmail.com>
2022-03-11 16:31:47 -0300

    requisito 8

--- 15-frontend-online-store/src/App.js
+++ 15-frontend-online-store/src/App.js
@@ -1,18 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
 import Cart from './pages/Cart';
+import Home from './pages/Home';
+import { getProductsFromCategoryAndQuery } from './services/api';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={ Home } />
-        <Route path="/cart" component={ Cart } />
-      </Switch>
-    </BrowserRouter>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      search: '',
+      products: [],
+      categoryId: '',
+      cartProducts: [],
+    };
+  }
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  };
+
+  getProducts = async () => {
+    const { categoryId, search } = this.state;
+    const apiResult = await getProductsFromCategoryAndQuery(categoryId, search);
+    this.setState({ products: apiResult.results });
+  };
+
+  handleCatClick = ({ target: { id } }) => {
+    this.setState({
+      categoryId: id,
+    }, this.getProducts);
+  }
+
+  handleAddToCart = ({ target: { id } }) => {
+    const { products } = this.state;
+    const objToSave = products.find((product) => product.id === id);
+    console.log(objToSave);
+    this.setState((prevState) => ({
+      cartProducts: [...prevState.cartProducts, objToSave],
+    }));
+  }
+
+  render() {
+    const { search, products, cartProducts } = this.state;
+
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={ (props) => (
+              <Home
+                { ...props }
+                handleChange={ this.handleChange }
+                handleCatClick={ this.handleCatClick }
+                handleAddToCart={ this.handleAddToCart }
+                getProducts={ this.getProducts }
+                search={ search }
+                products={ products }
+              />
+            ) }
+          />
+          <Route
+            path="/cart"
+            render={ (props) => <Cart { ...props } cartProducts={ cartProducts } /> }
+          />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- 15-frontend-online-store/src/components/ProductCard.js
+++ 15-frontend-online-store/src/components/ProductCard.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
 class ProductCard extends Component {
   // A exibição deve ter título, foto e preço.
   render() {
-    const { title, thumbnail, price } = this.props;
+    const { title, thumbnail, price, handleAddToCart, btnId } = this.props;
 
     return (
       <div data-testid="product">
         <img src={ thumbnail } alt={ title } />
         <div>{ title }</div>
         <div>{ price }</div>
+        <button
+          data-testid="product-add-to-cart"
+          type="button"
+          id={ btnId }
+          onClick={ handleAddToCart }
+        >
+          Adicionar ao carrinho
+        </button>
       </div>
     );
   }
@@ -20,6 +28,8 @@ ProductCard.propTypes = {
   title: PropTypes.string.isRequired,
   thumbnail: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
+  handleAddToCart: PropTypes.func.isRequired,
+  btnId: PropTypes.string.isRequired,
 };
 
 export default ProductCard;
