f02e69ab7d9320204ca61a8579006216798f5c41
Rafael Barbosa <rafaelbarbosa2k19@hotmail.com>
2022-03-11 17:10:55 -0300

    ajuste na home, propTypes sendo importado duas vezes

--- 15-frontend-online-store/src/pages/Home.js
+++ 15-frontend-online-store/src/pages/Home.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Categories from '../components/Categories';
 import ProductCard from '../components/ProductCard';
