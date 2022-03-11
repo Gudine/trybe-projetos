1b505dd35db8e4b043ee9dad23a831594199c6c8
Rafael Barbosa <rafaelbarbosa2k19@hotmail.com>
2022-03-11 17:49:37 -0300

    requisito 9

--- 15-frontend-online-store/src/pages/ProductPage.js
+++ 15-frontend-online-store/src/pages/ProductPage.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class ProductPage extends Component {
   render() {
-    const { match: { params: { product } } } = this.props;
+    const { match: { params: { product } }, handleAddToCart } = this.props;
     const productObj = JSON.parse(product);
-    const { title, thumbnail_id: thumbnailId, price, attributes } = productObj;
+    const { title, thumbnail_id: thumbnailId, price, attributes, btnId } = productObj;
 
     return (
       <div className="product-page">
+        <Link to="/cart" data-testid="shopping-cart-button">icone carrinho</Link>
         <span data-testid="product-detail-name">{ title }</span>
         <img
           src={ `http://http2.mlstatic.com/D_${thumbnailId}-I.jpg` }
@@ -20,6 +22,14 @@ class ProductPage extends Component {
             <li key={ id }>{`${name}: ${valueName}`}</li>
           ))}
         </ul>
+        <button
+          data-testid="product-detail-add-to-cart"
+          type="button"
+          id={ btnId }
+          onClick={ handleAddToCart }
+        >
+          Adicionar ao carrinho
+        </button>
       </div>
     );
   }
@@ -31,6 +41,7 @@ ProductPage.propTypes = {
       product: PropTypes.string.isRequired,
     }).isRequired,
   }).isRequired,
+  handleAddToCart: PropTypes.func.isRequired,
 };
 
 export default ProductPage;
