457f6d76b0ee7fe11bc71fd65bdca7d132e1a8a4
Gudine <gudine283@gmail.com>
2022-03-27 16:44:22 -0300

    Conserta bugs relacionados a API e manifest

--- 14-trybetunes/src/services/musicsAPI.js
+++ 14-trybetunes/src/services/musicsAPI.js
@@ -1,5 +1,5 @@
 const getMusics = async (id) => {
-  const request = await fetch(`https://itunes.apple.com/lookup?id=${id}`);
+  const request = await fetch(`https://itunes.apple.com/lookup?id=${id}&entity=song`);
   const requestJson = await request.json();
   return requestJson.results;
 };
